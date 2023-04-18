e4fcb55557d4504dc23730aefb6dbf05a47a54a8
lidgnulinux <lidgnulinux@gmail.com>
2023-04-18 16:49:59 +0700

    Fungsi move_to_center

--- tinywl.c
+++ tinywl.c
@@ -292,6 +292,50 @@ maximize(struct tinywl_server *server)
 }
 
 static void
+move_to_center(struct tinywl_server *server)
+{
+	struct wlr_surface *surface;
+	struct wlr_output *output;
+	struct tinywl_output *out;
+	struct tinywl_view *view;
+	struct wlr_seat *seat;
+
+	seat = server->seat;
+
+	surface = seat->keyboard_state.focused_surface;
+	if (!surface)
+		return;
+
+	view = view_from_surface(server, surface);
+
+	view->sx = view->x;
+	view->sy = view->y;
+	view->sw = view->w;
+	view->sh = view->h;
+
+	if (view->x < 0 || view->y < 0)
+	{
+		view->x = 0;
+		view->y = 0;
+	}
+
+	out = output_at(server, view->x, view->y);
+	output = out->wlr_output;
+
+	if ((view->x + view->w) > output->width || (view->y + view->h) > output->height)
+	{
+		view->x = 0;
+		view->y = 0;
+	}
+
+	view->x = (output->width - view->w) / 2;
+	view->y = (output->height - view->h) / 2;
+
+	wlr_scene_node_set_position(&view->scene_tree->node, view->x, view->y);
+	wlr_scene_node_raise_to_top(&view->scene_tree->node);
+}
+
+static void
 snap_edge_lr(struct tinywl_server *server, int factor)
 {
 	struct wlr_surface *surface;
@@ -701,6 +745,9 @@ static bool handle_keybinding(struct tinywl_server *server, xkb_keysym_t sym) {
 		break;
 	case XKB_KEY_percent:
 		half_width_height(server);
+		break;
+	case XKB_KEY_C:
+		move_to_center(server);
 		break;
 	default:
 		return false;
